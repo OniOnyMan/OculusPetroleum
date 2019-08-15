5568d66a27e1f13b03d7e2974ca2ccb0727ed6ba
OniOnyMan <theproteks.gamer@gmail.com>
2019-08-15 14:47:03 +0400

    RopeUpdater тест
    Пошёл проверять

---
+++ Assets/My Assets/Scripts/RopeConectController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeConectController : MonoBehaviour
+{
+    public string TargetTag = "ElevatorRopeConnector";
+
+    private Transform _targetPoint;
+    private Vector3 _previousPosition;
+
+    private Vector3 _targetPointAsChild
+    {
+        get
+        {
+            return transform.InverseTransformPoint(_targetPoint.position);
+        }
+    }
+
+    private LineRenderer _line
+    {
+        get
+        {
+            return GetComponent<LineRenderer>();
+        }
+    }
+
+    private void Start()
+    {
+        _targetPoint = GameObject.FindGameObjectWithTag(TargetTag).transform;
+        _previousPosition = transform.position;
+        SetConnectToTarget();
+    }
+
+    private void Update()
+    {
+        if (_previousPosition != transform.position)
+        {
+            SetConnectToTarget();
+        }
+    }    
+
+    private void SetConnectToTarget()
+    {
+        _line.SetPosition(1, _targetPointAsChild);
+    }       
+}
